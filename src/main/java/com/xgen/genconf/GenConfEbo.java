bc886437368577208de7079c8f115fdbec89c1b3
wanjiashengfo <493000667@qq.com>
2020-04-28 03:36:55 +0800

    第4次提交

---
+++ src/main/java/com/xgen/genconf/GenConfEbo.java
@@ -0,0 +1,18 @@
+package com.xgen.genconf;
+
+import com.xgen.genconf.vo.GenConfModel;
+import com.xgen.genconf.vo.ModuleConfModel;
+
+import java.util.Map;
+
+public class GenConfEbo implements GenConfEbi {
+    public GenConfModel getGenConf() {
+        //获取相应的配置数据
+        return null;
+    }
+
+    public Map<String, ModuleConfModel> getModuleConf() {
+        //获取相应的配置数据
+        return null;
+    }
+}
